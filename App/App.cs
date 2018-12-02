46f950c1cbb37294ceb507a7ea7284fbb17d91f6
Steven Mao <31294078+nzrugby@users.noreply.github.com>
2018-12-02 20:17:23 +1300

    add ConsoleUI function

--- App/App.cs
+++ App/App.cs
@@ -3,6 +3,7 @@ using MyDataIO;
 using MyDataPreProcessor;
 using MyServices;
 using System;
+using System.Collections.Generic;
 
 namespace MyApp
 {
@@ -11,13 +12,65 @@ namespace MyApp
         private static IDataIO dataIO = DataIO.GetDataIO();
         private static IDataPreProcessor dataPreProcessor = DataPreProcessor.GetProcessor();
         private static IServices service = Services.GetServices();
-
+                
         public void GeneratePayslip(string fileName)
         {
+            var OutputOption = GenerateUI();
             var stringLines = dataIO.ReadFile(fileName);
             var staffs = dataPreProcessor.GenerateStaffList(stringLines);
             var payslips = service.GeneratePayslips(staffs);
-            dataIO.Output(payslips, OUTPUTTO.BOTH);
-        }        
+            dataIO.Output(payslips, OutputOption);
+        }   
+        
+        private OUTPUTTO GenerateUI()
+        {
+            Console.WriteLine("                     WELCOME TO PAYSLIP                     ");
+            Console.WriteLine("============================================================");
+            Console.WriteLine(  "Please choose one of following options for operation:");
+            Console.WriteLine(  "1. Console output only");
+            Console.WriteLine(  "2. File output only");
+            Console.WriteLine(  "3. Both above");
+            Console.WriteLine(  "q. Quit");
+            Console.WriteLine("============================================================");
+            Console.Write("Please choose 1,2,3 or q for payslip operation :");
+            return SelectOption();
+        }
+
+        private OUTPUTTO SelectOption()
+        {
+            var arr = new List<char> { '1', '2', '3', 'q' };
+            char c;
+            do
+            {
+                Console.Write("Please choose right option : ");
+                c = Console.ReadKey().KeyChar;
+                if (c == 'q')
+                {
+                    Environment.Exit(0);
+                }
+                Console.WriteLine();
+            } while (!arr.Contains(c));
+            return GetOutputOption(int.Parse(c.ToString()));
+        }
+
+        private OUTPUTTO GetOutputOption(int input)
+        {
+            if(input == 1)
+            {
+                return OUTPUTTO.CONSOLE;
+            }
+            else if(input == 2)
+            {
+                return OUTPUTTO.FILE;
+            }
+            else if(input == 3)
+            {
+                return OUTPUTTO.BOTH;
+            }
+            else
+            {
+                return OUTPUTTO.CONSOLE;
+            }
+        }
     }
 }
--- Payslip/Program.cs
+++ Payslip/Program.cs
@@ -18,31 +18,6 @@ namespace Payslip
                 var app = new App();
                 var file = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "payslip", "data", "input.csv");
                 app.GeneratePayslip(file);
-
-                // use string input directly
-                //service.GetOutputConsole("David", "Rudd", "60050", "11%", "01 March-31March");
-                //Console.WriteLine("============================================================");
-
-
-
-                // use IStaff object for input
-                IDataIO dataIO = DataIO.GetDataIO();
-                List<string> input = dataIO.ReadFile(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "payslip", "data", "input.csv"));
-                IDataPreProcessor processor = DataPreProcessor.GetProcessor();
-                List<IStaff> staff = processor.GenerateStaffList(input);
-                Services service = Services.GetServices();
-                List<IPayslip> payslips = service.GeneratePayslips(staff);
-                dataIO.Output(payslips, OUTPUTTO.CONSOLE);
-
-                Console.ReadKey();
-                //service.GetOutputConsole("David", "Rudd", "60050", null, "01 March-31March");
-                //Console.WriteLine("============================================================");
-                //service.GetOutputConsole("David", "Rudd", "60050", "-0.01", "01 March-31March");
-                //Console.WriteLine("============================================================");
-                //service.GetOutputConsole("David", "Rudd", "-60050", null, "March-31March");
-                //Console.WriteLine("============================================================");
-                //service.GetOutputConsole("David", "Rudd", "60050", "0.12", "01 March-31");
-                //Console.WriteLine("============================================================");
             }
             catch (Exception e)
             {
